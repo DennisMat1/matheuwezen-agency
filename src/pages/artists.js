27e365f9cbea8ddddb78b908a4e49bfe87a54293
Dennis Matheuwezen <dennis.matheuwezen@student.ap.be>
2023-01-06 04:10:02 +0100

    Artists page: opmaak en styling

--- src/pages/artists.js
+++ src/pages/artists.js
@@ -1,25 +1,38 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+// imports
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Artist from "../../components/artist"
 
 const ArtistsPage = ({
   data: {
     allWpArtist: { edges },
+    wpPage: { artistsFields },
   },
 }) => {
+  const image = getImage(artistsFields.picture.localFile)
   return (
     <Layout pageTitle="Artists of Inghelbrecht Agency">
-      {edges.map(item => {
-        const artist = item.node.artistMeta
-        return (
-          <p key={item.node.id}>
-            {artist.firstName} {artist.lastName}
-          </p>
-        )
-      })}
+      <GatsbyImage image={image} alt={artistsFields.picture.altText} />
+      <section>
+        <h2>{artistsFields.title}</h2>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: artistsFields.description,
+          }}
+        />
+        <div>
+          {edges.map(({ node: artist }) => (
+            <Artist key={artist.id} slug={artist.slug} artist={artist} />
+          ))}
+        </div>
+      </section>
     </Layout>
   )
 }
+
+y
 
 export const query = graphql`
   query {
